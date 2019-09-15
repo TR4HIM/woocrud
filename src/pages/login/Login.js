ba951fd212f4e6671fce43421944866e3d2ebe24
Trahim Soufiane <tr4him@gmail.com>
2019-09-16 00:51:11 +0100

    Convert Login to hooks

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {  useState , useEffect } from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import Paper from '@material-ui/core/Paper';
@@ -9,143 +9,89 @@ import {login , storeUserData} from './actions';
 import { APP_ROUTES } from '../../config';
 import SiteLoader from '../../components/SiteLoader';
 import API from './server-effect';
+import { withRouter } from 'react-router-dom';
 
-class Login extends Component {
-
-    constructor(props){
-        super(props);
-
-        this.state = {
-            username : "admin",
-            password : "admin"
-        }
-    } 
-
-    static getDerivedStateFromProps(props,state) {
-        
-        if(props.AUTHORIZED){
+const Login = ({dispatch, AUTHORIZED , history }) => {
+     
+    const [username, setUsername] = useState('admin');
+    const [password, setPassword] = useState('admin');
+   
+    useEffect(() => {
+        if(AUTHORIZED){
             // REDIRECT TO PRODUCTS PAGE
-            props.history.push(APP_ROUTES.MY_PRODUCTS);
+            history.push(APP_ROUTES.MY_PRODUCTS);
         }
-        return null;
-    }
+    }, []);
 
-    keyPressHandler(e){
+    const keyPressHandler = (e) => {
         if(e.keyCode === 13)
-            this.login();
+        submitFormLogin();
     }
 
-    login(){
-
-        // SHOW LOADER
-        this.props.loading( true, 'login-loader' );
-
+    const submitFormLogin = (e) => {
+        e.preventDefault();
+        dispatch(loading( true, 'login-loader' ));
         let payload = {
-            username : this.username.value,
-            password : this.password.value
+            username , password 
         };
-
-
-         
-
         API.LOGIN(payload)
             .then((data)=>{
-                //Store User Category
-
                 localStorage.setItem('woo-app', JSON.stringify(data));
-
-                
-                this.props.login(true);
-
-                //Store User Category
-                this.props.storeUserData(data);
-
-                // HIDE LOADER
-                this.props.loading( false , 'login-loader');
-
-                // REDIRECT TO PRODUCTS PAGE
-                this.props.history.push(APP_ROUTES.MY_PRODUCTS);
-                
+                dispatch(login(true));
+                dispatch(storeUserData(data));
+                dispatch(loading( false, 'login-loader' ));
+                history.push(APP_ROUTES.MY_PRODUCTS);
             })
             .catch((error)=>{
-
-                this.props.dispatch({
+                dispatch({
                     type : 'ERROR',
                     payload : error
                 });
-
                 // HIDE LOADING
-                this.props.loading( false, 'login-loader' );
+                loading( false, 'login-loader' );
             })
     }
-    
-    handleChange = name => event => {
-        this.setState({
-            [name] : event.target.value
-        });
-    };
 
-    render() {
-        return (
-            <div id="login-page">
-                <SiteLoader id="login-loader" />
-                <div id="login-form" elevation={1}>
-                    <img id="logo" src={`${process.env.PUBLIC_URL}/img/logo.png`} alt="kibo" />
-                    <Paper id="paper" elevation={1}>
-                        <form  className="form" >
-                            <TextField
-                                inputRef={(username) => this.username = username}
-                                label="User name"
-                                value={this.state.username}
-                                onChange={this.handleChange('username')}
-                                onKeyDown={(e)=>this.keyPressHandler(e)}
-                                margin="normal"
-                                variant="outlined"
-                                fullWidth
-                                InputLabelProps={{ shrink: true }}
-                            />
-                            <TextField
-                                inputRef={(password)=>this.password = password}
-                                label="Password"
-                                value={this.state.password}
-                                onChange={this.handleChange('password')}
-                                onKeyDown={(e)=>this.keyPressHandler(e)}
-                                margin="normal"
-                                variant="outlined"
-                                fullWidth
-                                type="password"
-                                InputLabelProps={{ shrink: true }}
-                            />
-                            <div className="action">
-                                <Button variant="contained" color="primary" size="large" onClick={this.login.bind(this)} >
-                                    Sign Up
-                                </Button>
-                            </div>
-                        </form>
-                    </Paper>
-                </div>
+    return (
+        <div id="login-page">
+            <SiteLoader id="login-loader" />
+            <div id="login-form" elevation={1}>
+                <img id="logo" src={`${process.env.PUBLIC_URL}/img/logo.png`} alt="kibo" />
+                <Paper id="paper" elevation={1}>
+                    <form  className="form" >
+                        <TextField
+                            label="Username"
+                            value={username}
+                            onChange    ={(e) => setUsername(e.target.value)}
+                            onKeyDown   ={(e) => keyPressHandler(e)}
+                            margin="normal"
+                            variant="outlined"
+                            fullWidth
+                            InputLabelProps={{ shrink: true }}
+                        />
+                        <TextField
+                            label="Password"
+                            value={password}
+                            onChange    ={(e) => setPassword(e.target.value)}
+                            onKeyDown   ={(e) => keyPressHandler(e)}
+                            margin="normal"
+                            variant="outlined"
+                            fullWidth
+                            type="password"
+                            InputLabelProps={{ shrink: true }}
+                        />
+                        <div className="action">
+                            <Button variant="contained" color="primary" size="large" onClick={ submitFormLogin } >
+                                Sign Up
+                            </Button>
+                        </div>
+                    </form>
+                </Paper>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-const mapStateToProps = (state) => {
-    return {
-        AUTHORIZED : state.AUTHORIZED
-    }
-}
+const mapStateToProps = ({ AUTHORIZED }) => ({ AUTHORIZED });
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        ...bindActionCreators({
-            loading,
-            login,
-            storeUserData
-        }, dispatch ),
-
-        dispatch
-    }
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default withRouter(connect(mapStateToProps)(Login)) ; 
