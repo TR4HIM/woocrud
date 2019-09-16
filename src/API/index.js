8b6cb6d7f3aacb05c27ce5a065dfa1352a6d2c4a
Trahim Soufiane <tr4him@gmail.com>
2019-09-16 17:48:27 +0100

    API FOLDER

---
+++ src/API/index.js
@@ -0,0 +1,70 @@
+import axios from 'axios';
+
+export const protocol  = (process.env.NODE_ENV === 'production') ? 'https://' : 'http://';
+export const endPoint  = (process.env.NODE_ENV === 'production') ? 'kibo.ma' : 'woocrud.test';
+
+
+// Wordpress API
+export const WP         = `${protocol + endPoint}/wp-json/wp/v2`;
+// WooCommerce API
+export const WC         = `${protocol + endPoint}/wp-json/wc/v3`;
+
+export const AUTH       = `${protocol + endPoint}/wp-json/jwt-auth/v1`;
+export const WC_VENDOR  = `${protocol + endPoint}/wp-json/wc-vendor/v1`;
+
+
+const LOGIN = (formData)=>{
+
+    return axios.post( `${AUTH}/token`, formData)
+            .then((tokenResult)=>{
+                return tokenResult.data
+            });
+
+}
+
+const TOKEN_VALIDATE = (token)=>{
+
+    return axios.post( `${AUTH}/token/validate`, null, {
+        headers : {
+            "Authorization" : `Bearer ${token}`
+        }
+    })
+    .then((result)=>{
+        return result.data;
+    })
+
+}
+
+const WC_getWooProducts = ()=>{
+
+    var call;
+
+    return (token, categoryId, pager, limit)=>{
+
+        if (call)
+            call.cancel();
+        
+        call = axios.CancelToken.source();
+        return axios.get( `${WC}/products`, {
+            headers : {
+                "Authorization" : `Bearer ${token}`
+            },
+            cancelToken: call.token
+        })
+        .then((result)=>{
+            return result.data;
+        })
+        .catch((error)=>{
+            if ( !axios.isCancel(error)) 
+                return error;
+        })
+    }
+    
+
+}
+
+export default {
+    LOGIN,
+    TOKEN_VALIDATE,
+    WC_getWooProducts
+}
