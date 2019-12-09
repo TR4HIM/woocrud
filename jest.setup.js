2544b9215c15dd6cbfffb50b9fce31d31399fc30
Trahim Soufiane <tr4him@gmail.com>
2019-12-09 18:19:27 +0100

    Implement testing

---
+++ jest.setup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Enzyme, { configure, shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+export { shallow, mount, render };
+export default Enzyme;
+
+
---
+++ src/components/__tests__/ButtonUpload.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from '../../../jest.setup'
+import ButtonUpload from '../button-upload/ButtonUpload';
+
+
+describe('Component : Button Upload', () => {
+  it('Renders Button Upload without multiple attribute', () => {
+    const typeImage = 'thumbnail';
+    const wrapper = shallow(<ButtonUpload typeImage={typeImage} />);
+
+    // Expect the wrapper object to be defined
+    expect(wrapper.find('.upload-image-holder')).toBeDefined();
+    expect(wrapper.find('input')).toBeDefined();
+    expect(wrapper.find('input').prop('multiple')).toEqual(false);
+  });
+
+  it('Renders a button upload to support multiple files', () => {
+    const typeImage = 'gallery';
+    const wrapper = shallow(<ButtonUpload typeImage={typeImage} />);
+
+    // Expect the wrapper object to be defined
+    expect(wrapper.find('.upload-image-holder')).toBeDefined();
+    expect(wrapper.find('input')).toBeDefined();
+    expect(wrapper.find('input').prop('multiple')).toEqual(true);
+  });
+
+  it('Should call prop function', () => {
+    const mockOnChange = jest.fn();
+    const wrapper = shallow(<ButtonUpload onChange={mockOnChange} />);
+    const event = {
+        preventDefault() {},
+        target: {
+          files: [
+            'dummyValue.something'
+          ]   
+      }
+    };
+    wrapper.find('input').simulate('change',event);
+    expect(mockOnChange).toBeCalledWith(event);
+  })
+});
